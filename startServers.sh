d546c16890fb6b125647635ed6eb47d516c40f65
larpid <pidde@rhrk.uni-kl.de>
2019-10-28 10:19:35 +0100

    first tests for a startscript

---
+++ startServers.sh
@@ -0,0 +1,6 @@
+#tango start &
+
+/home/chamber7/bin/cs-studio/cs-studio-4.4.1-SNAPSHOT/cs-studio &
+/opt/TangoServers/SurfaceConceptTDC/SurfaceConceptTDC i0 ; fg
+#python3 PIStageTango.py 117018374 &
+
