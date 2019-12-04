fbb22e5edbd7bbc6050168b430cce8af2fd8dfec
larpid <pidde@rhrk.uni-kl.de>
2019-12-04 19:39:44 +0100

    new pressure controller tango server ready (not yet completely tested)

--- MVC3GaugeController.py
+++ MVC3GaugeController.py
@@ -1,6 +1,5 @@
 """
 device control class for the VACOM MVC3 vacuum gauge controller
-so far only used on devices with serial: todo:add these
 """
 
 import serial
@@ -8,8 +7,8 @@ import TangoHelper
 
 
 class MVC3GaugeController:
-    def __init__(self, serial_number):
-        self.serial_number = serial_number
+    def __init__(self, serial_address):
+        self.serial_address = serial_address
         self.ser = serial.Serial()
 
     def connect(self):
@@ -23,7 +22,21 @@ class MVC3GaugeController:
                                  parity=serial.PARITY_NONE,
                                  stopbits=1)
 
-        if not TangoHelper.connect_by_serial_number(self.ser, self.serial_number, idn_message='*IDN?\r'):
+        def idn_answer_check_function(idn_line, serial_address):
+            if len(idn_line) > 0:
+                print('Connection call uses Serial Address instead of S/N on this device')
+                if int(idn_line.strip(), 16) == serial_address:  # data is 1 byte
+                    return True
+            return False
+
+        # connection on this device is not done by asking for the serial number (not supported by device) but by
+        # asking for the RS485 Serial Address (can also be set/requested in the here used RS232 mode). These were
+        # manually set with SSA (e.g. 'RSA46\r' sets this to hex value 46 which is 70 in decimal) command and are
+        # labeled on the case of each device. IMPORTANT: after setting the serial address the device must be switched
+        # to RS485 and can then be switched back to RS232 immediately. Otherwise the address will reset on next device
+        # restart
+        if not TangoHelper.connect_by_serial_number(self.ser, serial_number=self.serial_address, idn_message='RSA\r',
+                                                    idn_answer_check_function=idn_answer_check_function):
             return False
 
         print('connection established')
@@ -35,11 +48,21 @@ class MVC3GaugeController:
 
     def read_pressure(self, channel):
         """read pressure of specified channel (can be 1,2 or 3)"""
-        self.ser.write(('RPV? %s\n' % channel).encode())
-        return float(self.ser.readline().decode().strip())
+        self.ser.write(('RPV%s\r' % channel).encode())
+        answer = self.ser.readline().decode().strip().split('\t')
+        if answer[0] == '?':
+            if answer[1] == 'S,':
+                # no sensor on this channel connected
+                return None
+            else:
+                print('ERROR: device reports error info %s' % answer)
+                return None
+        return float(answer[1])
 
 
 if __name__ == "__main__":
     """test code"""
-    gc1 = MVC3GaugeController(0)
+    gc1 = MVC3GaugeController(70)
     gc1.connect()
+    print(gc1.read_pressure(2))
+    gc1.disconnect()
--- MVC3GaugeControllerTango.py
+++ MVC3GaugeControllerTango.py
@@ -1,35 +1,37 @@
-#TODO: set up as needed...
 """
-Tango Server for the Elektro-Automatik PS2342-10B Power Supply
-so far only used on device with serial: 2845070119
+Tango Server for the VACOM MVC3 vacuum gauge controller
 """
 
-from EAPS import EAPS
+from MVC3GaugeController import MVC3GaugeController
 import sys
 from serial.serialutil import SerialException
-from tango import AttrWriteType, AttrQuality
-from PyTango import DevState, DebugIt, CmdArgType, Attr, Util
+from tango import AttrWriteType
+from PyTango import DevState
 from PyTango.server import run
 from PyTango.server import Device, DeviceMeta
+from PyTango.server import device_property
 from PyTango.server import attribute, command, pipe
 from TangoHelper import StoreStdOut
 
 
-class EAPSTango(Device, metaclass=DeviceMeta):
+class MVC3GaugeControllerTango(Device, metaclass=DeviceMeta):
+
+    serial_address = device_property(dtype=float)  # store address in decimal (can be 1 - 126)
 
     def init_device(self):
         sys.stdout = StoreStdOut()
-        self.eaps = EAPS(sys.argv[1])
-
-        # space for instance variables
-
+        self.mvc3 = MVC3GaugeController(self.serial_address)
         self.set_state(DevState.OFF)
         self.connect()  # try to auto connect on server start. can also be done manually
+
+    @attribute(dtype=str)
+    def server_message(self):
+        return sys.stdout.read_stored_message()
 
     @command
     def connect(self):
         self.set_state(DevState.INIT)
-        if self.eaps.connect():
+        if self.mvc3.connect():
             self.set_state(DevState.ON)
         else:
             self.set_state(DevState.FAULT)
@@ -41,7 +43,7 @@ class EAPSTango(Device, metaclass=DeviceMeta):
 
     @command
     def disconnect(self):
-        self.eaps.disconnect()
+        self.mvc3.disconnect()
         self.set_state(DevState.OFF)
 
     cmd_disconnect = attribute(access=AttrWriteType.WRITE)
@@ -49,166 +51,18 @@ class EAPSTango(Device, metaclass=DeviceMeta):
     def write_cmd_disconnect(self, _):
         self.disconnect()
 
-    c1_remote_control_active = attribute(access=AttrWriteType.WRITE,
-                                      dtype=bool)
+    @attribute(dtype=float, unit='mBar', access=AttrWriteType.READ)
+    def P1(self):
+        return self.mvc3.read_pressure(1)
 
-    def write_c1_remote_control_active(self, new_status):
-        if new_status:
-            self.eaps.switch_to_remote_control(1)
-        else:
-            self.eaps.switch_to_manual_control(1)
+    @attribute(dtype=float, unit='mBar', access=AttrWriteType.READ)
+    def P2(self):
+        return self.mvc3.read_pressure(2)
 
-    c2_remote_control_active = attribute(access=AttrWriteType.WRITE,
-                                         dtype=bool)
-
-    def write_c2_remote_control_active(self, new_status):
-        if new_status:
-            self.eaps.switch_to_remote_control(2)
-        else:
-            self.eaps.switch_to_manual_control(2)
-
-    c1_output_on = attribute(access=AttrWriteType.WRITE,
-                             dtype=bool)
-
-    def write_c1_output_on(self, new_status):
-        if new_status:
-            self.eaps.switch_output_on(1)
-        else:
-            self.eaps.switch_output_off(1)
-
-    c2_output_on = attribute(access=AttrWriteType.WRITE,
-                             dtype=bool)
-
-    def write_c2_output_on(self, new_status):
-        if new_status:
-            self.eaps.switch_output_on(2)
-        else:
-            self.eaps.switch_output_off(2)
-
-    c1_set_voltage = attribute(access=AttrWriteType.WRITE,
-                               dtype=float)
-
-    def write_c1_set_voltage(self, voltage):
-        self.eaps.write_set_voltage(1, voltage)
-
-    c2_set_voltage = attribute(access=AttrWriteType.WRITE,
-                               dtype=float)
-
-    def write_c2_set_voltage(self, voltage):
-        self.eaps.write_set_voltage(2, voltage)
-
-    c1_set_current = attribute(access=AttrWriteType.WRITE,
-                               dtype=float)
-
-    def write_c1_set_current(self, current):
-        self.eaps.write_set_current(1, current)
-
-    c2_set_current = attribute(access=AttrWriteType.WRITE,
-                               dtype=float)
-
-    def write_c2_set_current(self, current):
-        self.eaps.write_set_current(2, current)
-
-    @pipe
-    def c1_status_plus_actual_values(self):
-        """read status info plus voltage and current on output 1
-        this is not implemented separate for all values due to the devices ask-all-in-one-request
-        communication protocol and it's slow response times
-        """
-
-        status, actual_voltage, actual_current = self.eaps.read_status_plus_actual_values(1)
-
-        return 'status_plus_actual_values', {**status,
-                                             'actual_voltage': actual_voltage,
-                                             'actual_current': actual_current}
-
-    @pipe
-    def c2_status_plus_actual_values(self):
-        """read status info plus voltage and current on output 2
-        this is not implemented separate for all values due to the devices ask-all-in-one-request
-        communication protocol and it's slow response times
-        """
-
-        status, actual_voltage, actual_current = self.eaps.read_status_plus_actual_values(2)
-
-        return 'status_plus_actual_values', {**status,
-                                             'actual_voltage': actual_voltage,
-                                             'actual_current': actual_current}
-
-    @pipe
-    def c1_status_plus_set_values(self):
-        """read status info plus voltage and current (set values) on output 1
-        this is not implemented separate for all values due to the devices ask-all-in-one-request
-        communication protocol and it's slow response times
-        """
-
-        status, set_voltage, set_current = self.eaps.read_status_plus_set_values(1)
-
-        return 'status_plus_set_values', {**status,
-                                          'set_voltage': set_voltage,
-                                          'set_current': set_current}
-
-    @pipe
-    def c2_status_plus_set_values(self):
-        """read status info plus voltage and current (set values) on output 2
-        this is not implemented separate for all values due to the devices ask-all-in-one-request
-        communication protocol and it's slow response times
-        """
-
-        status, set_voltage, set_current = self.eaps.read_status_plus_set_values(2)
-
-        return 'status_plus_set_values', {**status,
-                                          'set_voltage': set_voltage,
-                                          'set_current': set_current}
-
-    @attribute(dtype=float, unit='U')
-    def c1_actual_voltage(self):
-        if self.get_state() == DevState.ON:
-            try:
-                _, voltage, _ = self.eaps.read_status_plus_actual_values(1)
-                return voltage
-            except SerialException:
-                self.set_state(DevState.FAULT)
-                self.eaps.disconnect()
-                print('ERROR: port not open')
-
-    @attribute(dtype=float, unit='U')
-    def c2_actual_voltage(self):
-        if self.get_state() == DevState.ON:
-            try:
-                _, voltage, _ = self.eaps.read_status_plus_actual_values(2)
-                return voltage
-            except SerialException:
-                self.set_state(DevState.FAULT)
-                self.eaps.disconnect()
-                print('ERROR: port not open')
-
-    @attribute(dtype=float, unit='A')
-    def c1_actual_current(self):
-        if self.get_state() == DevState.ON:
-            try:
-                _, _, current = self.eaps.read_status_plus_actual_values(1)
-                return current
-            except SerialException:
-                self.set_state(DevState.FAULT)
-                self.eaps.disconnect()
-                print('ERROR: port not open')
-
-    @attribute(dtype=float, unit='A')
-    def c2_actual_current(self):
-        if self.get_state() == DevState.ON:
-            try:
-                _, _, current = self.eaps.read_status_plus_actual_values(2)
-                return current
-            except SerialException:
-                self.set_state(DevState.FAULT)
-                self.eaps.disconnect()
-                print('ERROR: port not open')
-
-    @attribute(dtype=str)
-    def server_message(self):
-        return sys.stdout.read_stored_message()
+    @attribute(dtype=float, unit='mBar', access=AttrWriteType.READ)
+    def P3(self):
+        return self.mvc3.read_pressure(3)
 
 
 if __name__ == "__main__":
-    run([EAPSTango])
+    run([MVC3GaugeControllerTango])
