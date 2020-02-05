f12da12596623cf55172823ef5bca39afdfa65b9
larpid <pidde@rhrk.uni-kl.de>
2020-02-05 19:51:40 +0100

    developing control keys script to autorun on pi and make tango instructions as keyboard hotkeys

--- c7controlKeys.py
+++ c7controlKeys.py
@@ -1,21 +1,47 @@
-from pynput import keyboard
+"""created by L. Pidde 01/2020
+this is a control script to send keypresses/holds on a raspberry pi to any tango servers
+-it works as a keylogger on the otherwise deactivated keyboard of the pi (tty1,2,3,4,5 and 6 are deactivated)
+-key are bound to actions by config file 'c7controlKeys.cfg' and send a PRESS, RELEASE and regular heartBEAT signals
+    to any receiving tango server
+-the receiving server must implement a controlKey_[action] command to be able to execute key hold actions
+-single press signals can call a normal tango command
+"""
+
+import keyboard
 import time
+from tango import DeviceProxy
+from threading import Lock
 
-def on_press(key):
-    if key == keyboard.Key.esc:
-        return False  # stop listener
-    try:
-        k = key.char  # single-char keys
-    except:
-        k = key.name  # other keys
-    if True: #k in ['1', '2', 'left', 'right']:  # keys of interest
-        # self.keys.append(k)  # store it in global-like variable
-        print('%s ' % str(k), end='')
-        #return False  # stop listener; remove this if want more keys
+active_beats_lock = Lock()
+active_beats = []
 
-listener = keyboard.Listener(on_press=on_press)
-listener.start()  # start to listen on a separate thread
-while True:
-    time.sleep(5)
-    print(";")
-listener.join()  # remove if main thread is polling self.keys
+
+def key_event(key):
+    """handles key=keyboard.KeyboardEvent events"""
+
+    if key.event_type == "down":
+        if key.name not in keys_held_down:
+            print('%s %s' % (key.name, key.event_type))
+            keys_held_down.append(key.name)
+    elif key.event_type == "up":
+        keys_held_down.remove(key.name)
+        print('%s %s' % (key.name, key.event_type))
+
+
+if __name__ == "__main__":
+    # listeners works in background (separate Thread)
+    keyboard.hook(key_event)
+
+    # initialize known events from cfg file
+    config_file = open('c7controlKeys.cfg', 'r')
+    for textline in config_file.readlines():
+        if textline[0] != '#' and len(textline) > 0:
+            #todo
+    config_file.close()
+
+    # keep alive
+    #keyboard.wait()
+    while True:
+        time.sleep(1)
+
+     #self.tdc_proxy = DeviceProxy(self.tdc_name)
