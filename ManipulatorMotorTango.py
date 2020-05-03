ec8b4222e2759b4192edfb3e7d42ebc0cc321307
Lars Pidde <lars.pidde@gmail.com>
2020-05-03 17:32:41 +0200

    finished manipulator motor tango server. not yet testet

--- ManipulatorMotorTango.py
+++ ManipulatorMotorTango.py
@@ -1,89 +1,165 @@
 """Tango server for the implementation of the movement functionalities for our manipulator (currently only the main motor)
 written by Lars Pidde, 01/2020"""
 
-from MVC3GaugeController import MVC3GaugeController
+from ManipulatorMotor import ManipulatorMotor
 import sys
+import time
+from threading import Thread, Lock
 from serial.serialutil import SerialException
 from tango import AttrWriteType
-from PyTango import DevState
-from PyTango.server import run
-from PyTango.server import Device, DeviceMeta
-from PyTango.server import device_property
-from PyTango.server import attribute, command, pipe
+import tango
+from tango import DevState
+from tango.server import run
+from tango.server import Device, DeviceMeta
+from tango.server import device_property
+from tango.server import attribute, command, pipe
 from TangoHelper import StoreStdOut
 
 
 class ManipulatorMotorTANGO(Device, metaclass=DeviceMeta):
 
-    # serial_address = device_property(dtype=int,  # store address in decimal (can be 1 - 126)
-    #                                  default_value=1,
-    #                                  doc="this is the devices serial address in decimal. Connection on this device is "
-    #                                      "not done by asking for the serial number (not supported by device) but by "
-    #                                      "asking for the RS485 Serial Address (can also be set/requested in the here "
-    #                                      "used RS232 mode). These were manually set with SSA (e.g. 'RSA46\r' sets this "
-    #                                      "to hex value 46 which is 70 in decimal) command and are labeled on the case "
-    #                                      "of each device. IMPORTANT: after setting the serial address the device must "
-    #                                      "be switched to RS485 and can then be switched back to RS232 immediately. "
-    #                                      "Otherwise the address will reset on next device restart")
+    speed = device_property(dtype=float,
+                            default_value=100,
+                            doc="standard mode rotation speed given as step pulses per second")
 
-    # pressure_decimal_places = device_property(dtype=int,
-    #                                           default_value=2,
-    #                                           doc="number of displayed decimal places on pressure readings")
+    speed_fast = device_property(dtype=float,
+                                 default_value=500,
+                                 doc="fast mode rotation speed given as step pulses per second.")
+
+    movement_chunk_duration = device_property(dtype=float,
+                                             default_value=50,
+                                             doc="time (in ms) of a single chunk of the continuous motion.\n" +
+                                             "no heartbeat checks are made in between.")
+
+    max_heartbeat_distance = device_property(dtype=float,
+                                             default_value=150,
+                                             doc="maximum time (in ms) since last received heartbeat.\n" +
+                                             "if time is exceeded continuous motion will be terminated.\n" +
+                                             "should be more than movement_chunk_duration")
 
     def init_device(self):
         sys.stdout = StoreStdOut()
-        # self.get_device_properties()  # otherwise self.serial will be None
-        # self.mvc3 = MVC3GaugeController(self.serial_address)
-        # self.set_state(DevState.OFF)
-        # self.connect()  # try to auto connect on server start. can also be done manually
+        self.active_controlKey_action = None
+        # needed to reliably setup the action. its methods dont need thread safety
+        self.active_controlKey_action_lock = Lock()
+        self.set_state(DevState.ON)
+        self.action_thread = Thread(target=self.action_thread_method, daemon=True)
+        self.init_device()
+
+    def action_thread_method(self):
+        if self.active_controlKey_action is not None:
+            action_survived = self.active_controlKey_action.execute_next_chunk()
+            if not action_survived:
+                with self.active_controlKey_action_lock:
+                    self.active_controlKey_action = None
 
     @attribute(dtype=str)
     def server_message(self):
         return sys.stdout.read_stored_message()
 
     @command(dtype_in=int)
-    def controlKey_motor_CW_slow(self, num):
-        print('TEST! %s' % num)
+    def controlKey_motor_CW(self, command_code):
+        def move_one_chunk():
+            ManipulatorMotor.move(True, self.speed, self.movement_chunk_duration)
 
-    # @command
-    # def connect(self):
-    #     self.get_device_properties()
-    #     self.set_state(DevState.INIT)
-    #     if self.mvc3.connect():
-    #         self.set_state(DevState.ON)
-    #     else:
-    #         self.set_state(DevState.FAULT)
-    #
-    # cmd_connect = attribute(access=AttrWriteType.WRITE)
-    #
-    # def write_cmd_connect(self, _):
-    #     self.connect()
-    #
-    # @command
-    # def disconnect(self):
-    #     self.mvc3.disconnect()
-    #     self.set_state(DevState.OFF)
-    #
-    # cmd_disconnect = attribute(access=AttrWriteType.WRITE)
-    #
-    # def write_cmd_disconnect(self, _):
-    #     self.disconnect()
-    #
-    # @attribute(dtype=str, #unit='mBar',
-    #            access=AttrWriteType.READ)
-    # def P1(self):
-    #     """writes out errors and info too"""
-    #     return self.mvc3.read_pressure(1, decimal_places=self.pressure_decimal_places)
-    #
-    # @attribute(dtype=str, unit='mBar', access=AttrWriteType.READ)
-    # def P2(self):
-    #     """writes out errors and info too"""
-    #     return self.mvc3.read_pressure(2, decimal_places=self.pressure_decimal_places)
-    #
-    # @attribute(dtype=str, unit='mBar', access=AttrWriteType.READ)
-    # def P3(self):
-    #     """writes out errors and info too"""
-    #     return self.mvc3.read_pressure(3, decimal_places=self.pressure_decimal_places)
+        if command_code == 0:
+            # start action
+            self.set_state(DevState.MOVING)
+            with self.active_controlKey_action_lock:
+                self.active_controlKey_action = ControlKeyAction("motor_CW",
+                                                                 move_one_chunk, self.max_heartbeat_distance)
+        elif command_code == 1:
+            # end action
+            self.set_state(DevState.ON)
+            with self.active_controlKey_action_lock:
+                self.active_controlKey_action = None
+        elif command_code == 2:
+            # update beat time
+            self.active_controlKey_action.heartbeat("motor_CW")
+
+    @command(dtype_in=int)
+    def controlKey_motor_CW_fast(self, command_code):
+        def move_one_chunk():
+            ManipulatorMotor.move(True, self.speed_fast, self.movement_chunk_duration)
+
+        if command_code == 0:
+            # start action
+            self.set_state(DevState.MOVING)
+            with self.active_controlKey_action_lock:
+                self.active_controlKey_action = ControlKeyAction("motor_CW_fast",
+                                                                 move_one_chunk, self.max_heartbeat_distance)
+        elif command_code == 1:
+            # end action
+            self.set_state(DevState.ON)
+            with self.active_controlKey_action_lock:
+                self.active_controlKey_action = None
+        elif command_code == 2:
+            # update beat time
+            self.active_controlKey_action.heartbeat("motor_CW_fast")
+
+    @command(dtype_in=int)
+    def controlKey_motor_CCW(self, command_code):
+        def move_one_chunk():
+            ManipulatorMotor.move(False, self.speed, self.movement_chunk_duration)
+
+        if command_code == 0:
+            # start action
+            self.set_state(DevState.MOVING)
+            with self.active_controlKey_action_lock:
+                self.active_controlKey_action = ControlKeyAction("motor_CCW",
+                                                                 move_one_chunk, self.max_heartbeat_distance)
+        elif command_code == 1:
+            # end action
+            self.set_state(DevState.ON)
+            with self.active_controlKey_action_lock:
+                self.active_controlKey_action = None
+        elif command_code == 2:
+            # update beat time
+            self.active_controlKey_action.heartbeat("motor_CCW")
+
+    @command(dtype_in=int)
+    def controlKey_motor_CCW_fast(self, command_code):
+        def move_one_chunk():
+            ManipulatorMotor.move(False, self.speed_fast, self.movement_chunk_duration)
+
+        if command_code == 0:
+            # start action
+            self.set_state(DevState.MOVING)
+            with self.active_controlKey_action_lock:
+                self.active_controlKey_action = ControlKeyAction("motor_CCW_fast",
+                                                                 move_one_chunk, self.max_heartbeat_distance)
+        elif command_code == 1:
+            # end action
+            self.set_state(DevState.ON)
+            with self.active_controlKey_action_lock:
+                self.active_controlKey_action = None
+        elif command_code == 2:
+            # update beat time
+            self.active_controlKey_action.heartbeat("motor_CCW_fast")
+
+
+class ControlKeyAction:
+    def __init__(self, name, chunk_method, max_heartbeat_distance_ms):
+        self.name = name
+        self.act = chunk_method  # provides one short (not dividable) chunk of the continuous action
+        self.max_heartbeat_distance = max_heartbeat_distance_ms/1000.0
+        self.last_heartbeat = time.time()
+        self.action_alive = True
+
+    def heartbeat(self, name):
+        if name == self.name:
+            self.last_heartbeat = time.time()
+
+    def execute_next_chunk(self):
+        """should repeatedly be called to get somewhat continuous motion. use return value to delete action"""
+        if self.action_alive and time.time() - self.last_heartbeat < self.max_heartbeat_distance:
+            self.act()
+            return True
+        else:
+            if not self.action_alive:
+                print("ERROR: tried to continue dead action")
+                self.action_alive = False
+            return False
 
 
 if __name__ == "__main__":
