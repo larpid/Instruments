3d283ffb5634fab52332abe5b1ea58bf6ca5da26
larpid <pidde@rhrk.uni-kl.de>
2020-05-05 17:12:46 +0200

    device can now be restarted from jive (background thread now terminates properly and causes no more trouble for future instances)

--- ManipulatorMotorTango.py
+++ ManipulatorMotorTango.py
@@ -1,10 +1,14 @@
-"""Tango server for the implementation of the movement functionalities for our manipulator (currently only the main motor)
+"""Tango server for the implementation of the main movement functionality for our manipulator
+(currently only the main motor)
+
+do not create multiple device instances from one server process/ on the same PI. this would not only make no sense,
+but could also grant unforeseen consequences
 written by Lars Pidde, 01/2020"""
 
 from ManipulatorMotor import ManipulatorMotor
 import sys
 import time
-from threading import Thread, Lock
+import threading
 from serial.serialutil import SerialException
 from tango import AttrWriteType
 import tango
@@ -39,21 +43,30 @@ class ManipulatorMotorTANGO(Device, metaclass=DeviceMeta):
 
     def init_device(self):
         sys.stdout = StoreStdOut()
+        print("starting device server instance")
         self.get_device_properties()  # without this all device properties are None
         self.active_controlKey_action = None
-        # needed to reliably setup the action. its methods dont need thread safety
-        self.active_controlKey_action_lock = Lock()
-        self.set_state(DevState.ON)
-        self.action_thread = Thread(target=self.action_thread_method, daemon=True)
+        self.active_controlKey_action_lock = threading.Lock()
+        self.device_stop_requested = threading.Event()
+        self.action_thread = threading.Thread(target=self.action_thread_method, daemon=True)
         self.action_thread.start()
+        self.set_state(DevState.ON)
+
+    def delete_device(self):
+        print("stopping device server instance")
+        self.device_stop_requested.set()
+        self.action_thread.join()
+        print("all clear")
 
     def action_thread_method(self):
-        while True:
-            if self.active_controlKey_action is not None:
-                action_survived = self.active_controlKey_action.execute_next_chunk()
-                if not action_survived:
-                    with self.active_controlKey_action_lock:
+        print("background movement thread started")
+        while not self.device_stop_requested.is_set():
+            with self.active_controlKey_action_lock:
+                if self.active_controlKey_action is not None:
+                    action_survived = self.active_controlKey_action.execute_next_chunk()
+                    if not action_survived:
                         self.active_controlKey_action = None
+        print("background movement thread terminated")
 
     @attribute(dtype=str)
     def server_message(self):
@@ -67,6 +80,7 @@ class ManipulatorMotorTANGO(Device, metaclass=DeviceMeta):
         if command_code == 1:
             # start action
             self.set_state(DevState.MOVING)
+            self.get_device_properties()
             with self.active_controlKey_action_lock:
                 self.active_controlKey_action = ControlKeyAction("motor_CW",
                                                                  move_one_chunk, self.max_heartbeat_distance)
@@ -88,6 +102,7 @@ class ManipulatorMotorTANGO(Device, metaclass=DeviceMeta):
         if command_code == 1:
             # start action
             self.set_state(DevState.MOVING)
+            self.get_device_properties()
             with self.active_controlKey_action_lock:
                 self.active_controlKey_action = ControlKeyAction("motor_CW_fast",
                                                                  move_one_chunk, self.max_heartbeat_distance)
@@ -109,6 +124,7 @@ class ManipulatorMotorTANGO(Device, metaclass=DeviceMeta):
         if command_code == 1:
             # start action
             self.set_state(DevState.MOVING)
+            self.get_device_properties()
             with self.active_controlKey_action_lock:
                 self.active_controlKey_action = ControlKeyAction("motor_CCW",
                                                                  move_one_chunk, self.max_heartbeat_distance)
@@ -130,6 +146,7 @@ class ManipulatorMotorTANGO(Device, metaclass=DeviceMeta):
         if command_code == 1:
             # start action
             self.set_state(DevState.MOVING)
+            self.get_device_properties()
             with self.active_controlKey_action_lock:
                 self.active_controlKey_action = ControlKeyAction("motor_CCW_fast",
                                                                  move_one_chunk, self.max_heartbeat_distance)
@@ -156,10 +173,10 @@ class ControlKeyAction:
         if name == self.name:
             self.last_heartbeat = time.time()
 
-    def execute_next_chunk(self):
+    def get_next_chunk(self):
         """should repeatedly be called to get somewhat continuous motion. use return value to delete action"""
         if self.action_alive and time.time() - self.last_heartbeat < self.max_heartbeat_distance:
-            self.act()
+            return self.act()
             return True
         else:
             if not self.action_alive:
